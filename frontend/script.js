87d9bf7d524dd259b692e5acddeeaff24acf8d83
Jacob Horne <jhorne1@uci.edu>
2025-07-13 14:37:24 -0700

    Updated frontend to connect to deployed backend

--- frontend/script.js
+++ frontend/script.js
@@ -6,7 +6,7 @@ document.getElementById("checkButton").addEventListener("click", async () => {
     return;
   }
 
-  const response = await fetch("http://127.0.0.1:8000/predict", {
+  const response = await fetch("https://sms-spam-api-3g2w.onrender.com/predict", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
